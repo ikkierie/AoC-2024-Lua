91b3f498add19c3d927ec7a021e16ba89b808683
ikkierie <ikkierie@protonmail.com>
2024-12-02 07:18:06 +0000

    day 2 - added combined solution

---
+++ 02/both.lua
@@ -0,0 +1,37 @@
+local function is_valid(ns)
+    local prev_sign
+    for i = 2, #ns do
+        local x, y = ns[i - 1], ns[i]
+        local diff = math.abs(x - y)
+        local sign = (x > y) and 1 or (x < y) and -1 or 0
+        if
+            (diff < 1 or diff > 3) or
+            (prev_sign and prev_sign ~= sign)
+        then
+            return false
+        end
+        prev_sign = sign
+    end
+    return true
+end
+
+local a, b = 0, 0
+for line in io.lines([[input.txt]]) do
+    local ns = {}
+    for n in line:gmatch("%d+") do
+        table.insert(ns, tonumber(n))
+    end
+    if is_valid(ns) then
+        a = a + 1
+    end
+    for i = 1, (#ns + 1) do
+        local ns = table.move(ns, 1, #ns, 1, {})
+        table.remove(ns, i)
+        if is_valid(ns) then
+            b = b + 1
+            break
+        end
+    end
+end
+print("Part A:", a)
+print("Part B:", b)
