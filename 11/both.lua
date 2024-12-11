7be5805c5cc39a76c05f885002344255f8dcb320
ikkierie <ikkierie@protonmail.com>
2024-12-11 14:43:12 +0000

    day 11 - FINALLY switched away from the naive solution for both.lua (see b.lua for naive)

--- 11/both.lua
+++ 11/both.lua
@@ -1,40 +1,38 @@
-local cache = {}
-local function chunk(n)
-    if not cache[n] then
-        local stones = { n }
-        for _ = 1, 25 do
-            local new = {}
-            for i = 1, #stones do
-                local x = stones[i]
-                local d = math.ceil(math.log(x + 1, 10))
-                if x == 0 then
-                    table.insert(new, 1)
-                elseif d % 2 == 0 then
-                    table.insert(new, x // math.tointeger(10^(d / 2)))
-                    table.insert(new, x %  math.tointeger(10^(d / 2)))
-                else
-                    table.insert(new, x * 2024)
-                end
+local function insert(buf, n, x)
+    buf[x] = (buf[x] or 0) + n
+end
+
+local function exec(buf, turns)
+    for _ = 1, turns do
+        local new = {}
+        for x, n in pairs(buf) do
+            local d = math.ceil(math.log(x + 1, 10))
+            if x == 0 then
+                insert(new, n, 1)
+            elseif d % 2 == 0 then
+                insert(new, n, x // math.tointeger(10 ^ (d / 2)))
+                insert(new, n, x %  math.tointeger(10 ^ (d / 2)))
+            else
+                insert(new, n, x * 2024)
             end
-            stones = new
         end
-        cache[n] = stones
+        buf = new
     end
-    return cache[n]
+    return buf
 end
 
-local a = 0
-for n in io.lines([[input.txt]], "*n") do
-    a = a + #chunk(n)
-end
-print("Part A:", a)
-
-local b = 0
-for n in io.lines([[input.txt]], "*n") do
-    for _, x in ipairs(chunk(n)) do
-        for _, y in ipairs(chunk(x)) do
-            b = b + #chunk(y)
-        end
+local function sum(buf)
+    local sum = 0
+    for _, n in pairs(buf) do
+        sum = sum + n
     end
+    return sum
 end
-print("Part B:", b)
+
+local input = {}
+for x in io.lines([[input.txt]], "*n") do
+    insert(input, 1, x)
+end
+
+print("Part A:", sum(exec(input, 25)))
+print("Part B:", sum(exec(input, 75)))
